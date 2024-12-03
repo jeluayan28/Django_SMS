76accd941fe62c129aeb805f92ef8103f19dd30d
Laawrr <lawrencekie8@gmail.com>
2024-12-03 12:01:31 +0800

    update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,14 +6,12 @@ def landing_page(request):
     if request.method == 'POST':
         form = MessageForm(request.POST)
         if form.is_valid():
-            # If form is valid, save the data and show success message
             form.save()
             messages.success(request, 'Message sent successfully!')
-            return redirect('landing_page')  # Redirect to the same page to clear the form
+            return redirect('landing_page') 
         else:
             messages.error(request, 'Failed to send the message. Please try again.')
     else:
-        # If it's a GET request, just render an empty form
         form = MessageForm()
 
     return render(request, 'landing_page.html', {'form': form})
